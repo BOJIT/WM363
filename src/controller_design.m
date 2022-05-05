1ebabd7ad2496779c7332251dee782c9cfc65c03
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-05-05 10:18:23 +0100

    Base template + submodule

---
+++ src/controller_design.m
@@ -0,0 +1,14 @@
+% FILE:         controller_design.m
+% DESCRIPTION:  Controller design example script
+% AUTHOR:       James Bennion-Pedley
+% DATE CREATED: 05/05/2022
+
+%------------------------------------------------------------------------------%
+
+close all; clc; clear;
+
+%-------------------------------- Entry Point ---------------------------------%
+
+
+
+%------------------------------ Helper Functions ------------------------------%
